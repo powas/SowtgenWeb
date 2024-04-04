3ea50d1fe64b40f01c381a340a700e2387b461d3
powas <pablopowazniak@gmail.com>
2024-04-04 16:47:21 -0300

    Dev

--- process-solutions.php
+++ process-solutions.php
@@ -14,13 +14,16 @@ include($_SERVER["DOCUMENT_ROOT"] . '/header.php');
     </div>
   </section>
   <section class="bg-primary">
-    <div class="container">
+    <div class="container position-relative z-1">
       <div class="row align-items-center">
-        <div class="col-lg-7 py-5 py-lg-6 text-white">
+        <div class="col-lg-6 py-5 py-lg-6 text-white">
           <img src="/img/sowtgen-process-solutions-logo-blanco.svg" alt="SOWTGEN Ingenieria y Obras" title="SOWTGEN Ingenieria y Obras Logo" class="mb-4">
           <p class="lead">Ésta es la unidad dedicada a brindar soluciones para el desarrollo de procesos y fabricación de equipos tecnológicos que intervienen en los procesos industriales.</p>
           <p>Sus profesionales se especializan en el desarrollo integral de sistemas de procesamiento para la producción de alimentos, bebidas y productos de home & personal care.</p>
           <p>La unidad, interviene en proyectos desde el diseño, la construcción e implementación de procesos y tecnologías desarrolladas brindado soluciones de última generación.</p>
+        </div>
+        <div class="col-lg-6 pb-5 pb-lg-0">
+          <img src="/img/home-hero.png" alt="SOWTGEN" title="SOWTGEN" class="img-fluid">
         </div>
       </div>
     </div>
@@ -40,37 +43,37 @@ include($_SERVER["DOCUMENT_ROOT"] . '/header.php');
       </div>
       <div class="row gy-5">
         <div class="col-lg-4 d-flex flex-column align-items-center text-center">
-            <img src="/img/ingenieria-y-obras-600x600.jpg" alt="Ingeniería" title="Ingeniería" class="img-fluid max-width-60per rounded-circle mb-4">
+            <img src="/img/montajes-electromecanicos-600x600.jpg" alt="Ingeniería" title="Ingeniería" class="img-fluid max-width-60per rounded-circle mb-4">
             <h3 class="mb-4">Ingeniería</h3>
             <p>Ingenieria SOWTGEN ofrece servicios de ingeniería que abarcan desde estudios de viabilidad hasta desarrollo de arquitectura, incluyendo simulación y automatizaciones.</p>
             <a href="/ingenieria-y-obras/ingenieria" type="button" class="btn btn-primary mt-auto">Conocer más</a>
         </div>
         <div class="col-lg-4 d-flex flex-column align-items-center text-center">
-            <img src="/img/ingenieria-y-obras-600x600.jpg" alt="Management" title="Management" class="img-fluid max-width-60per rounded-circle mb-4">
+            <img src="/img/montajes-electromecanicos-600x600.jpg" alt="Management" title="Management" class="img-fluid max-width-60per rounded-circle mb-4">
             <h3 class="mb-4">Management</h3>
             <p>Profesionales de Project & Program Management gestionan proyectos, supervisando documentación, presupuesto y cronogramas establecidos en la etapa de ingeniería conceptual.</p>
             <a href="/ingenieria-y-obras/management" type="button" class="btn btn-primary mt-auto">Conocer más</a>
         </div>
         <div class="col-lg-4 d-flex flex-column align-items-center text-center">
-            <img src="/img/ingenieria-y-obras-600x600.jpg" alt="Fabricación" title="Fabricación" class="img-fluid max-width-60per rounded-circle mb-4">
+            <img src="/img/montajes-electromecanicos-600x600.jpg" alt="Fabricación" title="Fabricación" class="img-fluid max-width-60per rounded-circle mb-4">
             <h3 class="mb-4">Fabricación</h3>
             <p>SOWTGEN tiene una planta propia con tecnología avanzada para fabricar estructuras y sistemas modulares de alta tecnificación.</p>
             <a href="/ingenieria-y-obras/fabricacion" type="button" class="btn btn-primary mt-auto">Conocer más</a>
         </div>
         <div class="col-lg-4 d-flex flex-column align-items-center text-center">
-            <img src="/img/ingenieria-y-obras-600x600.jpg" alt="Montajes Electromecánicos" title="Montajes Electromecánicos" class="img-fluid max-width-60per rounded-circle mb-4">
+            <img src="/img/montajes-electromecanicos-600x600.jpg" alt="Montajes Electromecánicos" title="Montajes Electromecánicos" class="img-fluid max-width-60per rounded-circle mb-4">
             <h3 class="mb-4">Montajes Electromecánicos</h3>
             <p>Su equipo especializado se enfoca en proyectos de construcción mecánica, eléctrica y automatización, con amplia experiencia en diversas industrias.</p>
             <a href="/ingenieria-y-obras/montajes-electromecanicos" type="button" class="btn btn-primary mt-auto">Conocer más</a>
         </div>
         <div class="col-lg-4 d-flex flex-column align-items-center text-center">
-            <img src="/img/ingenieria-y-obras-600x600.jpg" alt="Automatización y Control" title="Automatización y Control" class="img-fluid max-width-60per rounded-circle mb-4">
+            <img src="/img/montajes-electromecanicos-600x600.jpg" alt="Automatización y Control" title="Automatización y Control" class="img-fluid max-width-60per rounded-circle mb-4">
             <h3 class="mb-4">Automatización y Control</h3>
             <p>Ingenieria La división de montaje industrial se enfoca en construcción mecánica, eléctrica y automatización.</p>
             <a href="/ingenieria-y-obras/automatizacion-y-control" type="button" class="btn btn-primary mt-auto">Conocer más</a>
         </div>
         <div class="col-lg-4 d-flex flex-column align-items-center text-center">
-            <img src="/img/ingenieria-y-obras-600x600.jpg" alt="Mantenimiento" title="Mantenimiento" class="img-fluid max-width-60per rounded-circle mb-4">
+            <img src="/img/montajes-electromecanicos-600x600.jpg" alt="Mantenimiento" title="Mantenimiento" class="img-fluid max-width-60per rounded-circle mb-4">
             <h3 class="mb-4">Mantenimiento</h3>
             <p>Ingenieria SOWTGEN ofrece servicios de mantenimiento de plantas, además de capacitación y diversas metodologías de gestión para reducir costos en áreas de mantenimiento.</p>
             <a href="/ingenieria-y-obras/mantenimiento" type="button" class="btn btn-primary mt-auto">Conocer más</a>
